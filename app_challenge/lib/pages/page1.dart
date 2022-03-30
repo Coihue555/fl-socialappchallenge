210e8166815d8ac09496ec9027af933c494dfb93
Coihue555 <kevlar555@gmail.com>
2022-03-29 22:32:34 -0300

    singlescrollview

--- app_challenge/lib/pages/page1.dart
+++ app_challenge/lib/pages/page1.dart
@@ -12,71 +12,57 @@ class MainPage extends StatelessWidget {
       backgroundColor: Colors.white,
       body: SafeArea(
         
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 300,
-                color: Colors.white,
-                child: StackHeader(),
-              ),
-          
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: const[
-                      ActivityCard('Walking', 'walk.png'),
-                      ActivityCard('Cycling', 'cycling.png'),
-                      
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: const[
-                      ActivityCard('Walking', 'walk.png'),
-                      ActivityCard('Cycling', 'cycling.png'),
-                      
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: const[
-                      ActivityCard('Walking', 'walk.png'),
-                      ActivityCard('Cycling', 'cycling.png'),
-                      
-                    ],
-                  ),
-                  
-                ],
-              ),
-              Container(
-                alignment: Alignment.center,
-                padding: EdgeInsets.all(5),
-                margin: EdgeInsets.all(5),
-                color: Colors.white,
-                height: 80,
-                width: double.infinity,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
-                    Icon(Icons.home, size: 40, color: Colors.black54,),
-                    Icon(Icons.restore, size: 40, color: Colors.black54,),
-                    Icon(Icons.person, size: 40, color: Colors.black54,),
-                    Icon(Icons.bar_chart_sharp, color: Colors.red, size: 40,),
-                    Icon(Icons.notifications_none_outlined, size: 40, color: Colors.black54,),
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              color: Colors.white,
+              child: StackHeader(),
+            ),
+        
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const[
+                        ActivityCard('Walking', 'walk.png'),
+                        ActivityCard('Cycling', 'cycling.png'),
+                        
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const[
+                        ActivityCard('Walking', 'walk.png'),
+                        ActivityCard('Cycling', 'cycling.png'),
+                        
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const[
+                        ActivityCard('Walking', 'walk.png'),
+                        ActivityCard('Cycling', 'cycling.png'),
+                        
+                      ],
+                    ),
+                    
                   ],
                 ),
-          
-              )
-            ],
-          ),
+              ),
+            ),
+            BottomMenu()
+          ],
         ),
       ),
     );
   }
 }
+
+
 
--- app_challenge/lib/widgets/widgets.dart
+++ app_challenge/lib/widgets/widgets.dart
@@ -2,3 +2,4 @@ export 'package:app_challenge/widgets/headers.dart';
 
 export 'package:app_challenge/widgets/stack_header.dart';
 export 'package:app_challenge/widgets/activity_card.dart';
+export 'package:app_challenge/widgets/bottom_menu.dart';
